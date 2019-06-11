b72d54df9620eab2b4da6a6aa56adba413f3d77e
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-11 20:24:42 +0100

    Implement basic PersonProvider

---
+++ Bloodbank.Core/Providers/IPersonProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using Bloodbank.Core.Models;
+
+namespace Bloodbank.Core.Providers
+{
+    public interface IPersonProvider
+    {
+        Person GetPerson(int id);
+        IEnumerable<Person> GetPeople<T>(Expression<Func<Person, bool>> predicate);
+    }
+}
---
+++ Bloodbank.Core/Providers/PersonProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Bloodbank.Core.Models;
+using Bloodbank.Core.Services;
+
+namespace Bloodbank.Core.Providers
+{
+    public class PersonProvider : IPersonProvider
+    {
+        private readonly IDataBase _db;
+        public PersonProvider(IDataBase db)
+        {
+            _db = db;
+        }
+
+
+        public Person GetPerson(int id)
+            => _db.RestoreSingle<Person>(x => x.Id == id);
+
+        public IEnumerable<Person> GetPeople<T>(Expression<Func<Person, bool>> predicate)
+            => _db.RestoreMany(predicate);
+
+        //Maybe Add store.... 
+    }
+}
