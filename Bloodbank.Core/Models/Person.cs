572214fe42cdb7cbd9cc0573c73ae9ee65bbfdd2
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-11 20:24:29 +0100

    Add ID to user

--- Bloodbank.Core/Models/Person.cs
+++ Bloodbank.Core/Models/Person.cs
@@ -7,6 +7,7 @@ namespace Bloodbank.Core.Models
 {
     public class Person
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Address Address { get; set; }
