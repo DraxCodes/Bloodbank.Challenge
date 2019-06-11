9c352290c6d90704ffaf182c7c9cf57d77750932
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-11 22:11:10 +0100

    abstract the Colorful.Console class

---
+++ Bloodbank.Core/SystemAbstractions/Console.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Drawing;
+
+namespace Bloodbank.Core.SystemAbstractions
+{
+    public class Console : IConsole
+    {
+        public void WriteLine(string message, Color color) => Colorful.Console.WriteLine(message, color);
+
+        public string ReadLine() => Colorful.Console.ReadLine();
+    }
+}
---
+++ Bloodbank.Core/SystemAbstractions/IConsole.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace Bloodbank.Core.SystemAbstractions
+{
+    public interface IConsole
+    {
+        void WriteLine(string message, Color color);
+        string ReadLine();
+    }
+}
