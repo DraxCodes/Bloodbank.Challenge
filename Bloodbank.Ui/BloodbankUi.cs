9d5e9fb9e6a8fbaffb9cff5fb17c38ab96c0dbbe
Joel Parkinson <joelparkinson@live.co.uk>
2019-06-11 22:52:56 +0100

    Multiple Additons See Desc.
    - Implement CancelationTokenSource for looping the console UI.
    - Implement Selection system.
    - Implement resgitration in the UI
    - Implement DisplayPersonInfo in the UI
    - Implement Display the ammount of entries from the DB in the UI
    - Implement default when selected choice isn't valid for in the UI
    - Implement Close UI feature.

--- Bloodbank.Ui/BloodbankUi.cs
+++ Bloodbank.Ui/BloodbankUi.cs
@@ -1,27 +1,65 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using Bloodbank.Core.Providers;
 using Bloodbank.Core.Services;
+using Bloodbank.Core.SystemAbstractions;
 
 namespace Bloodbank.Ui
 {
     public class BloodbankUi
     {
         private readonly ILogger _logger;
+        private readonly IConsole _console;
         private readonly IPersonService _personService;
-        private readonly IPersonProvider _personProvider;
+        private CancellationTokenSource cancellationToken = new CancellationTokenSource();
 
-        public BloodbankUi(ILogger logger, IPersonService personService, IPersonProvider personProvider)
+        public BloodbankUi(ILogger logger, IConsole console, IPersonService personService)
         {
             _logger = logger;
+            _console = console;
             _personService = personService;
-            _personProvider = personProvider;
         }
-        public async Task InitializeUi()
+        public Task InitializeUi()
         {
-            _logger.Log("Working I guess.");
-            _personService.RegisterPerson();
+            //Todo: Remove this shite maybe
+            _logger.Write("Welcome to the Bloodbank.\n");
+            while (!cancellationToken.Token.IsCancellationRequested)
+            {
+                _logger.Write("Please select which option you require.\n" +
+                              "1: Register\n" +
+                              "2: Lookup person\n" +
+                              "3: Count Entries\n" +
+                              "4: Close\n");
+                var selection = _console.ReadLine();
 
-            await Task.Delay(-1);
+                //Todo: check for issues.
+                var selectedInt = int.Parse(selection);
+                SwitchSelection(selectedInt);
+            }
+
+            return Task.CompletedTask;;
+        }
+
+        public void SwitchSelection(int selection)
+        {
+            switch (selection)
+            {
+                case 1:
+                    _personService.RegisterPerson();
+                    break;
+                case 2:
+                    _personService.DisplayPersonInformation();
+                    break;
+                case 3:
+                    _personService.DisplayEntries();
+                    break;
+                case 4:
+                    cancellationToken.Cancel();
+                    break;
+                default:
+                    _logger.Log("Incorrect Selection");
+                    break;;
+            }
         }
 
         
